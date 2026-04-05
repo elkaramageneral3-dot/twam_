e31aa923764f30ee06358731e3ad93d481994e87
elkaramageneral3-dot <elkaramageneral3@gmail.com>
2026-04-05 01:41:17 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => ({
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './mgx'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   server: {
